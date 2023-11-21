e3e83eef2dbe4d889090e60824fdbf594b17849f
KaiAsaoka <kasaoka@student.ubc.ca>
2023-11-21 00:40:31 -0800

    first commit

---
+++ build/2023_competition/enph353/enph353_gazebo/catkin_generated/enph353_gazebo-msg-extras.cmake.develspace.in
@@ -0,0 +1,2 @@
+set(enph353_gazebo_MESSAGE_FILES "")
+set(enph353_gazebo_SERVICE_FILES "/home/fizzer/ros_ws/src/2023_competition/enph353/enph353_gazebo/srv/GetLegalPlates.srv;/home/fizzer/ros_ws/src/2023_competition/enph353/enph353_gazebo/srv/SubmitPlate.srv")
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,15 @@
+# CMake generated Testfile for 
+# Source directory: /home/fizzer/ros_ws/src
+# Build directory: /home/fizzer/ros_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("2023_competition/adeept_awr/adeept_awr")
+subdirs("2023_competition/adeept_awr/adeept_awr_description")
+subdirs("2023_competition/enph353/enph353")
+subdirs("2023_competition/enph353/enph353_npcs")
+subdirs("2023_competition/enph353/enph353_utils")
+subdirs("2023_competition/adeept_awr_ros_driver")
+subdirs("2023_competition/adeept_awr/adeept_awr_gazebo")
+subdirs("2023_competition/enph353/enph353_gazebo")
