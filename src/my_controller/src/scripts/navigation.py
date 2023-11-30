2a7088ad7ca41dbabf55936a42bddfe90560cdb0
AveryWong03 <averyw15444@gmail.com>
2023-11-29 21:22:24 -0800

    cleaning

--- src/my_controller/src/scripts/navigation.py
+++ src/my_controller/src/scripts/navigation.py
@@ -11,6 +11,7 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import sys
 from geometry_msgs.msg import Twist
+import csv
 
 
 ##
@@ -41,15 +42,19 @@ class navigation():
 
     def callback(self, data):
 
+        WIDTH = 600
+        HEIGHT = 400
         
         frame = self.bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
         
+        # Apply blue color mask
+
         hsv_image = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
-        lower_blue = np.array([115, 128, 95])
-        upper_blue = np.array([120, 255, 204])
+        lower_blue = np.array([90, 50, 30])
+        upper_blue = np.array([120, 255, 120])
         blue_mask = cv2.inRange(hsv_image, lower_blue, upper_blue)
-         
-        # Find contours in the binary mask
+        
+        # Find contours
         contours, _ = cv2.findContours(blue_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
        
         if contours:
@@ -61,34 +66,16 @@ class navigation():
         contour = frame.copy()
         cv2.drawContours(contour, [approx], 0, (0, 255, 0), 2)
 
-        x0, y0 = approx[0][0][0], approx[0][0][1]
-        x2, y2 = approx[2][0][0], approx[2][0][1]
-
-        if(x0 < x2):
-            tl = approx[0]
-            tr = approx[3]
-            bl = approx[1]  
-            br = approx[2]
-        else:
-            tl = approx[1]
-            tr = approx[0]
-            bl = approx[2]  
-            br = approx[3]
+        tl, tr, bl, br = rectangle_positions(approx)
             
         pts1 = np.float32([tl,tr,bl,br])
-        pts2 = np.float32([[0,0],[600,0],[0,400],[600,400]])
+        pts2 = np.float32([[0,0],[WIDTH,0],[0,HEIGHT],[WIDTH,HEIGHT]])
 
         perspective_matrix = cv2.getPerspectiveTransform(pts1, pts2)
         
-        # Apply the perspective transform
-        dst = cv2.warpPerspective(frame, perspective_matrix, (600, 400))
-        dstmask = cv2.warpPerspective(blue_mask, perspective_matrix, (600, 400))
-        letters, _ = cv2.findContours(dstmask, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
-        
-        lettermask = dst.copy()
-        letterimage = cv2.drawContours(lettermask, letters, -1, (0, 255, 0), 1)
-        cv2.imshow("Letter Image", cv2.cvtColor(letterimage, cv2.COLOR_RGB2BGR))
+        dst = cv2.warpPerspective(frame, perspective_matrix, (WIDTH, HEIGHT))
 
+        # Display 
         cv2.imshow("Contour Crop", cv2.cvtColor(dst, cv2.COLOR_RGB2BGR))
         cv2.waitKey(1)
         
@@ -101,9 +88,17 @@ class navigation():
         cv2.imshow("Contour Image", cv2.cvtColor(contour, cv2.COLOR_RGB2BGR))
         cv2.waitKey(1)
 
-        
+        csv_file_path = '/home/fizzer/ros_ws/src/2023_competition/enph353/enph353_gazebo/scripts/plates.csv'
 
-        
+        # Open the CSV file and read its contents
+        with open(csv_file_path, 'r') as file:
+            # Create a CSV reader object
+            csv_reader = csv.reader(file)
+
+            # Read the data from the CSV file
+            for row in csv_reader:
+                print(row)
+
     ##
     # @brief Listener method to retrieve data from ROS camera
         ##NAVIGATION 
@@ -112,93 +107,66 @@ class navigation():
         roi_x1, roi_y1, roi_x2, roi_y2 = 0, 400, 1280, 410  # Adjust these coordinates as needed
         ## Default Resolution x = 320, y = 240
 
-        # ## Crop the image to the ROI
-        # roi_image = frame[roi_y1:roi_y2, roi_x1:roi_x2]
-        # cv2.waitKey(1)
+        ## Crop the image to the ROI
+        roi_image = frame[roi_y1:roi_y2, roi_x1:roi_x2]
+        cv2.waitKey(1)
         
-        # hsv_image = cv2.cvtColor(roi_image, cv2.COLOR_RGB2HSV)
-        # lower_white = np.array([0, 0, 200])
-        # upper_white = np.array([255, 30, 255])
-        # white_mask = cv2.inRange(hsv_image, lower_white, upper_white)
-        # ## Define the lower and upper bounds for the color you want to detect (here, it's blue)
-        # sensitivity = 15
-        # cv2.waitKey(1)
-        # ## Define a threshold value for detecting grayscale change
-        # threshold_value = 100  # Adjust this threshold as needed
+        hsv_image = cv2.cvtColor(roi_image, cv2.COLOR_RGB2HSV)
+        lower_white = np.array([0, 0, 200])
+        upper_white = np.array([255, 30, 255])
+        white_mask = cv2.inRange(hsv_image, lower_white, upper_white)
+        ## Define the lower and upper bounds for the color you want to detect (here, it's blue)
+        sensitivity = 15
+        cv2.waitKey(1)
+        ## Define a threshold value for detecting grayscale change
+        threshold_value = 100  # Adjust this threshold as needed
 
-        # ## Find contours in the binary mask
-        # pidcontours, _ = cv2.findContours(white_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        ## Find contours in the binary mask
+        pidcontours, _ = cv2.findContours(white_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-        # cx = 0
-        # cx = 0
+        cx = 0
+        cx = 0
 
-        # pid_img = cv2.drawContours(frame, pidcontours, -1, (0, 255, 0), 1)
+        pid_img = cv2.drawContours(frame, pidcontours, -1, (0, 255, 0), 1)
 
-        # cxnet = 0
-        # cynet = 0
-        # moments = 0
-        # cxavg = 640
-        # ## Iterate through the contours and find the position of color change within the ROI
-        # for contour in pidcontours:
+        ## Iterate through the contours and find the position of color change within the ROI
+        for contour in pidcontours:
 
-        #     ## Calculate the centroid of the contour
-        #     M = cv2.moments(contour)
+            ## Calculate the centroid of the contour
+            M = cv2.moments(contour)
 
-        #     if M["m00"] != 0:
-        #         cx = int(M["m10"] / M["m00"])
+            if M["m00"] != 0:
+                cx = int(M["m10"] / M["m00"])
+                cy = int(M["m01"] / M["m00"])
 
-        #         ## Add the ROI offset to get the position within the original image
-        #         cx += roi_x1
+                ## Add the ROI offset to get the position within the original image
                 
-        #         cxnet += cx
-        #         moments += 1
+                cx += roi_x1
+                cy += roi_y1
 
-        #         #print(f"Position of color change within ROI: ({cx}, {cy})")
-        # if moments != 0:
-        #     cxavg = cxnet / moments
+                #print(f"Position of color change within ROI: ({cx}, {cy})")
 
-        # rate = rospy.Rate(2)
-        # move = Twist()
-        # move.linear.x = .1
-        
-        # cv2.rectangle(frame, (10, 2), (100,20), (255,255,255), -1)
+        rate = rospy.Rate(2)
+        move = Twist()
+        move.linear.x = .1
 
-        # if cxavg >= 0 and cxavg < 256:
-        #     move.angular.z = 2
-        #     cv2.putText(frame, str(cxavg)+" LEFT", (15, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
+        if(cx<640):
+            move.angular.z = .5
+        else:
+            move.angular.z = -.5
 
-        # elif cxavg >= 256 and cxavg < 512:
-        #     move.angular.z = 0.75
-        #     cv2.putText(frame, str(cxavg)+" left", (15, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
-
-        # elif cxavg >= 512 and cxavg < 728:
-        #     move.angular.z = 0
-        #     cv2.putText(frame, str(cxavg)+" Straight", (15, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
-
-        # elif cxavg >= 728 and cxavg < 1024:
-        #     move.angular.z = -0.75
-        #     cv2.putText(frame, str(cxavg)+" right", (15, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
-
-        # else:
-        #     move.angular.z = -2
-        #     cv2.putText(frame, str(cxavg)+" RIGHT", (15, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
-
-            
-        # center_coordinates = (int(cxavg), int(h))  # Change the coordinates as needed
-        # #print (center_coordinates)
-        # radius = 30
-        # color = (0, 0, 255)  # Red color in BGR format
-        # thickness = -1 # Thickness of the circle's border (use -1 for a filled circle)
-        # # Process the frame here (you can add your tracking code or other operations)
-        # frame_with_circle = cv2.circle(frame, center_coordinates, radius, color, thickness)
-
-
-
-        # cv2.imshow("PID", cv2.cvtColor(frame_with_circle, cv2.COLOR_RGB2BGR))
-        # self.move_pub.publish(move)
+        cv2.imshow("PID", cv2.cvtColor(pid_img, cv2.COLOR_RGB2BGR))
+        self.move_pub.publish(move)
         
         
-    
+def rectangle_positions(approx):
+    x0, y0 = approx[0][0][0], approx[0][0][1]
+    x2, y2 = approx[2][0][0], approx[2][0][1]
+    if(x0 < x2):
+        return approx[0],approx[3],approx[1],approx[2]
+    else:
+        return approx[1],approx[0],approx[2],approx[3]
+
 def main(args):
     rospy.init_node('listener', anonymous=True)
     controller = navigation()
