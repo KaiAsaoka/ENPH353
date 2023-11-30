ca6cc8a1cfa049e85b7058c63b06407e0875ebfd
AveryWong03 <averyw15444@gmail.com>
2023-11-29 20:09:51 -0800

    added cropping for signs

--- src/my_controller/src/scripts/navigation.py
+++ src/my_controller/src/scripts/navigation.py
@@ -35,93 +35,62 @@ class navigation():
 
         rospy.Subscriber("/R1/pi_camera/image_raw", Image, self.callback)
   
-        
+
     def callback(self, data):
 
         
         frame = self.bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
-
+        
         hsv_image = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
         lower_blue = np.array([90, 50, 30])
         upper_blue = np.array([120, 255, 120])
         blue_mask = cv2.inRange(hsv_image, lower_blue, upper_blue)
 
-        grayframe = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)  # trainimage
-
-        kp_grayframe, desc_grayframe = self.sift.detectAndCompute(grayframe, None)
-
-        matches = self.flann.knnMatch(self.desc_image, desc_grayframe, k=2)
-
-        good_points = []
-        
-        
-        ### Contours garbage
-
-         
         # Find contours in the binary mask
-        #contours, _ = cv2.findContours(blue_mask, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
         contours, _ = cv2.findContours(blue_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        
-        c_img = cv2.drawContours(frame, contours, -1, (0, 255, 0), 1)
        
         if contours:
-            # Find the largest rectangular contour
+
             largest_contour = max(contours, key=cv2.contourArea)
 
-            # Get the bounding rectangle of the largest contour
-            x, y, w, h = cv2.boundingRect(largest_contour)
+        epsilon = 0.02 * cv2.arcLength(largest_contour, True)
+        approx = cv2.approxPolyDP(largest_contour, epsilon, True)
+        contour = frame.copy()
+        cv2.drawContours(contour, [approx], 0, (0, 255, 0), 2)
 
-            # Crop the image to the bounding rectangle
-            cropped_img = c_img[y:y+h, x:x+w]
-            cv2.imshow("cropped image", cv2.cvtColor(cropped_img, cv2.COLOR_RGB2BGR))
-            cv2.waitKey(1)    
+        x0, y0 = approx[0][0][0], approx[0][0][1]
+        x2, y2 = approx[2][0][0], approx[2][0][1]
 
-        pts1 = np.float32([[y,x],[y,x+w],[y+h,x],[y+h,x+w]])
-        pts2 = np.float32([[0,0],[0,600],[400,0],[400,600]])
-        M = cv2.getPerspectiveTransform(pts1,pts2)
-        #perspective = frame.copy()
-        dst = cv2.warpPerspective(cropped_img,M,(600,400))
+        if(x0 < x2):
+            tl = approx[0]
+            tr = approx[3]
+            bl = approx[1]  
+            br = approx[2]
+        else:
+            tl = approx[1]
+            tr = approx[0]
+            bl = approx[2]  
+            br = approx[3]
+            
+        pts1 = np.float32([tl,tr,bl,br])
+        pts2 = np.float32([[0,0],[600,0],[0,400],[600,400]])
 
+        perspective_matrix = cv2.getPerspectiveTransform(pts1, pts2)
+        
+        # Apply the perspective transform
+        dst = cv2.warpPerspective(frame, perspective_matrix, (600, 400))
 
-        cv2.imshow("contours", dst)
+        cv2.imshow("Contour Crop", cv2.cvtColor(dst, cv2.COLOR_RGB2BGR))
         cv2.waitKey(1)
         
-        ###
-        
-        for m, n in matches:
-            if m.distance < 0.25 * n.distance:
-                good_points.append(m)
+        cv2.imshow("Image window", cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+        cv2.waitKey(1)
 
-        if len(good_points) > 4:
-            query_pts = np.float32([self.kp_image[m.queryIdx].pt for m in good_points]).reshape(-1, 1, 2)
-            train_pts = np.float32([kp_grayframe[m.trainIdx].pt for m in good_points]).reshape(-1, 1, 2)
+        cv2.imshow("Binary", blue_mask)
+        cv2.waitKey(1)
 
-            matrix, _ = cv2.findHomography(query_pts, train_pts, cv2.RANSAC, 5.0)
-
-            # Perspective transform
-            h, w = self.img.shape
-            pts = np.float32([[0, 0], [0, h], [w, h], [w, 0]]).reshape(-1, 1, 2)
-
-            dst = cv2.perspectiveTransform(pts, matrix)
-
-            homography = cv2.polylines(frame, [np.int32(dst)], True, (255, 0, 0), 3)
-
-            cv2.imshow("Image window", cv2.cvtColor(homography, cv2.COLOR_RGB2BGR))
-            cv2.waitKey(1)
-
-            cv2.imshow("Binary", blue_mask)
-            cv2.waitKey(1)
-            
-            
-
-
-        else:
-            cv2.imshow("Image window", cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
-            cv2.waitKey(1)
-
-            cv2.imshow("Binary", blue_mask)
-            cv2.waitKey(1)
-
+        cv2.imshow("Contour Image", cv2.cvtColor(contour, cv2.COLOR_RGB2BGR))
+        cv2.waitKey(1)
 
     ##
     # @brief Listener method to retrieve data from ROS camera
