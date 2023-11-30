65a4c2939805bd2f0326ca610ffb35b618247456
KaiAsaoka <kasaoka@student.ubc.ca>
2023-11-29 19:05:22 -0800

    PID working (but not well)

--- src/my_controller/src/scripts/navigation.py
+++ src/my_controller/src/scripts/navigation.py
@@ -10,6 +10,8 @@ import numpy as np
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import sys
+from geometry_msgs.msg import Twist
+
 
 ##
 # @brief Callback method
@@ -31,6 +33,7 @@ class navigation():
 		## Features
         self.kp_image, self.desc_image = self.sift.detectAndCompute(self.img, None)
         self.bridge = CvBridge()
+        self.move_pub = rospy.Publisher("/R1/cmd_vel",Twist,queue_size=1)
         print("Loaded template image file: " + self.template_path)
 
         rospy.Subscriber("/R1/pi_camera/image_raw", Image, self.callback)
@@ -62,7 +65,7 @@ class navigation():
         #contours, _ = cv2.findContours(blue_mask, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
         contours, _ = cv2.findContours(blue_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         
-        c_img = cv2.drawContours(frame, contours, -1, (0, 255, 0), 1)
+        # c_img = cv2.drawContours(frame, contours, -1, (0, 255, 0), 1)
        
         if contours:
             # Find the largest rectangular contour
@@ -72,15 +75,16 @@ class navigation():
             x, y, w, h = cv2.boundingRect(largest_contour)
 
             # Crop the image to the bounding rectangle
-            cropped_img = c_img[y:y+h, x:x+w]
-            cv2.imshow("cropped image", cv2.cvtColor(cropped_img, cv2.COLOR_RGB2BGR))
+            #cropped_img = c_img[y:y+h, x:x+w]
+            #cv2.imshow("cropped image", cv2.cvtColor(cropped_img, cv2.COLOR_RGB2BGR))
             cv2.waitKey(1)    
 
         pts1 = np.float32([[y,x],[y,x+w],[y+h,x],[y+h,x+w]])
         pts2 = np.float32([[0,0],[0,600],[400,0],[400,600]])
+        
         M = cv2.getPerspectiveTransform(pts1,pts2)
         #perspective = frame.copy()
-        dst = cv2.warpPerspective(cropped_img,M,(600,400))
+        dst = cv2.warpPerspective(frame, M, (600,400))
 
 
         cv2.imshow("contours", dst)
@@ -122,9 +126,63 @@ class navigation():
             cv2.imshow("Binary", blue_mask)
             cv2.waitKey(1)
 
+        ##NAVIGATION 
 
-    ##
-    # @brief Listener method to retrieve data from ROS camera
+        ## Define the coordinates of the region of interest (ROI)
+        roi_x1, roi_y1, roi_x2, roi_y2 = 0, 400, 1280, 410  # Adjust these coordinates as needed
+        ## Default Resolution x = 320, y = 240
+
+        ## Crop the image to the ROI
+        roi_image = frame[roi_y1:roi_y2, roi_x1:roi_x2]
+        cv2.waitKey(1)
+        
+        hsv_image = cv2.cvtColor(roi_image, cv2.COLOR_RGB2HSV)
+        lower_white = np.array([0, 0, 200])
+        upper_white = np.array([255, 30, 255])
+        white_mask = cv2.inRange(hsv_image, lower_white, upper_white)
+        ## Define the lower and upper bounds for the color you want to detect (here, it's blue)
+        sensitivity = 15
+        cv2.waitKey(1)
+        ## Define a threshold value for detecting grayscale change
+        threshold_value = 100  # Adjust this threshold as needed
+
+        ## Find contours in the binary mask
+        pidcontours, _ = cv2.findContours(white_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        cx = 0
+        cx = 0
+
+        pid_img = cv2.drawContours(frame, pidcontours, -1, (0, 255, 0), 1)
+
+        ## Iterate through the contours and find the position of color change within the ROI
+        for contour in pidcontours:
+
+            ## Calculate the centroid of the contour
+            M = cv2.moments(contour)
+
+            if M["m00"] != 0:
+                cx = int(M["m10"] / M["m00"])
+                cy = int(M["m01"] / M["m00"])
+
+                ## Add the ROI offset to get the position within the original image
+                
+                cx += roi_x1
+                cy += roi_y1
+
+                #print(f"Position of color change within ROI: ({cx}, {cy})")
+
+        rate = rospy.Rate(2)
+        move = Twist()
+        move.linear.x = .1
+
+        if(cx<640):
+            move.angular.z = .5
+        else:
+            move.angular.z = -.5
+
+        cv2.imshow("PID", cv2.cvtColor(pid_img, cv2.COLOR_RGB2BGR))
+        self.move_pub.publish(move)
+        
         
     
 def main(args):
