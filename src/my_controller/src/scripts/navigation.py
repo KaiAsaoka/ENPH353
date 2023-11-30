93a6c3fdcbbe2ac85edc11a4d5bd3e97e0520521
KaiAsaoka <kasaoka@student.ubc.ca>
2023-11-29 20:16:25 -0800

    pid updates

--- src/my_controller/src/scripts/navigation.py
+++ src/my_controller/src/scripts/navigation.py
@@ -79,16 +79,16 @@ class navigation():
             #cv2.imshow("cropped image", cv2.cvtColor(cropped_img, cv2.COLOR_RGB2BGR))
             cv2.waitKey(1)    
 
-        pts1 = np.float32([[y,x],[y,x+w],[y+h,x],[y+h,x+w]])
-        pts2 = np.float32([[0,0],[0,600],[400,0],[400,600]])
+            pts1 = np.float32([[y,x],[y,x+w],[y+h,x],[y+h,x+w]])
+            pts2 = np.float32([[0,0],[0,600],[400,0],[400,600]])
         
-        M = cv2.getPerspectiveTransform(pts1,pts2)
-        #perspective = frame.copy()
-        dst = cv2.warpPerspective(frame, M, (600,400))
+            M = cv2.getPerspectiveTransform(pts1,pts2)
+            #perspective = frame.copy()
+            dst = cv2.warpPerspective(frame, M, (600,400))
 
 
-        cv2.imshow("contours", dst)
-        cv2.waitKey(1)
+            cv2.imshow("contours", dst)
+            cv2.waitKey(1)
         
         ###
         
@@ -126,62 +126,96 @@ class navigation():
             cv2.imshow("Binary", blue_mask)
             cv2.waitKey(1)
 
-        ##NAVIGATION 
+        # ##NAVIGATION 
+        # h = 500
+        # ## Define the coordinates of the region of interest (ROI)
+        # roi_x1, roi_y1, roi_x2, roi_y2 = 0, h, 1280, h+5  # Adjust these coordinates as needed
+        # ## Default Resolution x = 320, y = 240
 
-        ## Define the coordinates of the region of interest (ROI)
-        roi_x1, roi_y1, roi_x2, roi_y2 = 0, 400, 1280, 410  # Adjust these coordinates as needed
-        ## Default Resolution x = 320, y = 240
-
-        ## Crop the image to the ROI
-        roi_image = frame[roi_y1:roi_y2, roi_x1:roi_x2]
-        cv2.waitKey(1)
+        # ## Crop the image to the ROI
+        # roi_image = frame[roi_y1:roi_y2, roi_x1:roi_x2]
+        # cv2.waitKey(1)
         
-        hsv_image = cv2.cvtColor(roi_image, cv2.COLOR_RGB2HSV)
-        lower_white = np.array([0, 0, 200])
-        upper_white = np.array([255, 30, 255])
-        white_mask = cv2.inRange(hsv_image, lower_white, upper_white)
-        ## Define the lower and upper bounds for the color you want to detect (here, it's blue)
-        sensitivity = 15
-        cv2.waitKey(1)
-        ## Define a threshold value for detecting grayscale change
-        threshold_value = 100  # Adjust this threshold as needed
+        # hsv_image = cv2.cvtColor(roi_image, cv2.COLOR_RGB2HSV)
+        # lower_white = np.array([0, 0, 200])
+        # upper_white = np.array([255, 30, 255])
+        # white_mask = cv2.inRange(hsv_image, lower_white, upper_white)
+        # ## Define the lower and upper bounds for the color you want to detect (here, it's blue)
+        # sensitivity = 15
+        # cv2.waitKey(1)
+        # ## Define a threshold value for detecting grayscale change
+        # threshold_value = 100  # Adjust this threshold as needed
 
-        ## Find contours in the binary mask
-        pidcontours, _ = cv2.findContours(white_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        # ## Find contours in the binary mask
+        # pidcontours, _ = cv2.findContours(white_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-        cx = 0
-        cx = 0
+        # cx = 0
+        # cx = 0
 
-        pid_img = cv2.drawContours(frame, pidcontours, -1, (0, 255, 0), 1)
+        # pid_img = cv2.drawContours(frame, pidcontours, -1, (0, 255, 0), 1)
 
-        ## Iterate through the contours and find the position of color change within the ROI
-        for contour in pidcontours:
+        # cxnet = 0
+        # cynet = 0
+        # moments = 0
+        # cxavg = 640
+        # ## Iterate through the contours and find the position of color change within the ROI
+        # for contour in pidcontours:
 
-            ## Calculate the centroid of the contour
-            M = cv2.moments(contour)
+        #     ## Calculate the centroid of the contour
+        #     M = cv2.moments(contour)
 
-            if M["m00"] != 0:
-                cx = int(M["m10"] / M["m00"])
-                cy = int(M["m01"] / M["m00"])
+        #     if M["m00"] != 0:
+        #         cx = int(M["m10"] / M["m00"])
 
-                ## Add the ROI offset to get the position within the original image
+        #         ## Add the ROI offset to get the position within the original image
+        #         cx += roi_x1
                 
-                cx += roi_x1
-                cy += roi_y1
+        #         cxnet += cx
+        #         moments += 1
 
-                #print(f"Position of color change within ROI: ({cx}, {cy})")
+        #         #print(f"Position of color change within ROI: ({cx}, {cy})")
+        # if moments != 0:
+        #     cxavg = cxnet / moments
 
-        rate = rospy.Rate(2)
-        move = Twist()
-        move.linear.x = .1
+        # rate = rospy.Rate(2)
+        # move = Twist()
+        # move.linear.x = .1
+        
+        # cv2.rectangle(frame, (10, 2), (100,20), (255,255,255), -1)
 
-        if(cx<640):
-            move.angular.z = .5
-        else:
-            move.angular.z = -.5
+        # if cxavg >= 0 and cxavg < 256:
+        #     move.angular.z = 2
+        #     cv2.putText(frame, str(cxavg)+" LEFT", (15, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
 
-        cv2.imshow("PID", cv2.cvtColor(pid_img, cv2.COLOR_RGB2BGR))
-        self.move_pub.publish(move)
+        # elif cxavg >= 256 and cxavg < 512:
+        #     move.angular.z = 0.75
+        #     cv2.putText(frame, str(cxavg)+" left", (15, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
+
+        # elif cxavg >= 512 and cxavg < 728:
+        #     move.angular.z = 0
+        #     cv2.putText(frame, str(cxavg)+" Straight", (15, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
+
+        # elif cxavg >= 728 and cxavg < 1024:
+        #     move.angular.z = -0.75
+        #     cv2.putText(frame, str(cxavg)+" right", (15, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
+
+        # else:
+        #     move.angular.z = -2
+        #     cv2.putText(frame, str(cxavg)+" RIGHT", (15, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
+
+            
+        # center_coordinates = (int(cxavg), int(h))  # Change the coordinates as needed
+        # #print (center_coordinates)
+        # radius = 30
+        # color = (0, 0, 255)  # Red color in BGR format
+        # thickness = -1 # Thickness of the circle's border (use -1 for a filled circle)
+        # # Process the frame here (you can add your tracking code or other operations)
+        # frame_with_circle = cv2.circle(frame, center_coordinates, radius, color, thickness)
+
+
+
+        # cv2.imshow("PID", cv2.cvtColor(frame_with_circle, cv2.COLOR_RGB2BGR))
+        # self.move_pub.publish(move)
         
         
     
